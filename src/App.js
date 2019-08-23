02b09faa8c97a4a3768a7422e894a37ec43f26e5
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-08-23 12:18:43 -0700

    Took out the JokeCard from the app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
 import "./App.css";
 import WelcomForms from "./components/WelcomeForms";
-import JokesCard from "./components/JokesCard";
 
 function App() {
   return (
     <div className="App">
       <WelcomForms />
-      <JokesCard
-        username="Cody"
-        jokeSetup="What concert costs just 45 cents?"
-        jokePunchline="50 Cent featuring Nickelback!"
-      />
     </div>
   );
 }
