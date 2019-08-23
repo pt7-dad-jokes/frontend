6aa6ea5ca92c68189179d92033fadf578f11fcc2
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-08-22 20:17:53 -0700

    Got the inital joke card setup. Takes in a username, the setup and the punchline

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
 import WelcomForms from "./components/WelcomeForms";
+import JokesCard from "./components/JokesCard";
 
 function App() {
   return (
     <div className="App">
       <WelcomForms />
+      <JokesCard
+        username="Cody"
+        jokeSetup="Why did the chicken cross the road?"
+        jokePunchline="To get to the KFC"
+      />
     </div>
   );
 }
