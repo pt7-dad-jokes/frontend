bbfd1338f1edd7b05593d3aaa1b216ab34137468
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-08-25 19:14:46 -0700

    Removed the profile page from the App.js for now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "./App.css";
 import WelcomForms from "./components/WelcomeForms";
-import ProfilePage from "./components/ProfilePage";
 
 function App() {
   return (
     <div className="App">
       <WelcomForms />
-      <ProfilePage username="CodyC" />
     </div>
   );
 }
