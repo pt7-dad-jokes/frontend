c19755c0af10294382a6ca11953bfe5118b38598
Josh <cowley.josh@gmail.com>
2019-08-28 22:24:26 -0400

    Fixed Conflict

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import WelcomForms from "./components/WelcomeForms";
-<<<<<<< HEAD
-import ProfilePage from "./components/ProfilePage";
-import VerticalNav from "./components/VerticalNav"
-=======
 import Dashboard from "./components/Dashboard";
->>>>>>> 8fe27a5943ae6ecfbccf666d2726ea6d6ea02928
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(localStorage.getItem("token"));
@@ -14,16 +9,7 @@ function App() {
   console.log(localStorage.getItem("token"));
   return (
     <div className="App">
-<<<<<<< HEAD
-      {loggedIn ? (
-        <ProfilePage username="Cody" />
-      ) : (
-        <WelcomForms setLoggedIn={setLoggedIn} />
-      )}
-<VerticalNav/>
-=======
       {loggedIn ? <Dashboard /> : <WelcomForms setLoggedIn={setLoggedIn} />}
->>>>>>> 8fe27a5943ae6ecfbccf666d2726ea6d6ea02928
     </div>
   );
 }
